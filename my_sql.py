37f2aee572cfd688d60c90c3cc819bbf507e7b01
m0L3n <m.lenz@mailbox.org>
2023-01-07 18:47:31 +0100

    add my_sql file

---
+++ my_sql.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import MySQLdb
+
+### INSERT Operation ###
+
+# DB connection
+db = MySQLdb.connect("")
+#cursor object
+cursor = db.cursor()
+# query to inserc record to db
+sql = """ INSERT INTO STUDENT(NAME,SURENAME, ROLL_NO)
+            VALUES('Torsten','Mueller',1)"""
+try:
+    #exec query
+    cursor.execute(sql)
+    #commit to db
+    db.commit()
+except:
+    #in case of any accuring error
+    db.rollback()
+
+# disconnect from server
+db.close
